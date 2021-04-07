4d804f07a195af1f88be65bf70ee9832b9497011
Daniil Timoschenko <carnolio@gmail.com>
2021-04-07 18:23:46 +0500

    Add files via upload

--- sorter.py
+++ sorter.py
@@ -2,8 +2,6 @@ import click,eyed3,shutil,os
 '''
 Реализовать консольное (CLI) приложение для сортировки музыкальных файлов по исполнителям и альбомам:
 программа анализирует файлы в исходной директории, считывает ID3-теги, извлекает из них информацию о названии трека, исполнителе и альбоме;
-
-
 программа должна принимать 3 ключа командной строки:
 --help - вывести справочное сообщение;
 -s | --src-dir - исходная директория, по умолчанию ".";
@@ -26,11 +24,10 @@ $ ./sorter.py
 Done.
 $ ./sorter.py --dst-dir=/home/user/some/directory
 ...
-
 '''
 @click.command()
-@click.option('-s', default='./', help='Source dir. Default = ./')
-@click.option('-d', default='./', help='Destination dir. Default = ./')
+@click.option('--sourceFolder','-s', default='./', help='Source dir. Default = ./')
+@click.option('--destFolder','-d', default='./', help='Destination dir. Default = ./')
 def hello():
     click.echo('Hello World!')
 
@@ -40,7 +37,7 @@ mp3s = os.listdir(path=srcDir)
 
 #print (os.name)
 def checkName (filename):
-    # escList = ['\\','/',':','*','<','>','|']
+    # escList = ['\\','/',':','*','<','>','|','?']
     fname=""
     escList = ['/', ':','*','?','<', '>', '|']
     for i in filename:
@@ -49,8 +46,9 @@ def checkName (filename):
         fname += i
     return fname
 
+hello()
 for mp3 in mp3s:
-    print("file: ", mp3)
+    #print("file: ", mp3)
     if os.name == "nt":
         delim = "\\"
     else:
@@ -59,22 +57,23 @@ for mp3 in mp3s:
     title, album, artist = "", "", ""
     isMove = True
     audiofile = eyed3.load(srcDir + delim + mp3)
-    print(audiofile)
-    if audiofile.tag.title != None:
-        title = audiofile.tag.title.encode("cp1252").decode("cp1251")
-        title = title.strip()
-    if audiofile.tag.artist != None:
-        artist = audiofile.tag.artist.encode("cp1252").decode("cp1251")
-        artist = artist.strip()
-    if audiofile.tag.album != None:
-        album = audiofile.tag.album.encode("cp1252").decode("cp1251")
-        album = album.strip()
+    #print(audiofile)
+    if audiofile and audiofile.tag:
+        if audiofile.tag.title != None:
+            title = audiofile.tag.title.encode("cp1252").decode("cp1251")
+            title = title.strip()
+        if audiofile.tag.artist != None:
+            artist = audiofile.tag.artist.encode("cp1252").decode("cp1251")
+            artist = artist.strip()
+        if audiofile.tag.album != None:
+            album = audiofile.tag.album.encode("cp1252").decode("cp1251")
+            album = album.strip()
 #except Exception:
     #print("No ID3 Tag")
 #finally:
-    print("title:"+title)
-    print("artist:"+artist)
-    print("album:"+album)
+    #print("title:"+title)
+    #print("artist:"+artist)
+    #print("album:"+album)
 
     #audiofile.tag.save()
 
@@ -91,9 +90,9 @@ for mp3 in mp3s:
     if isMove:
         newFileName = checkName(title+" - "+artist+" - "+album+".mp3")
         newPath = checkName(srcDir + delim + artist + delim + album + delim)
-        print(newPath + newFileName)
-        print("try create dir:" + newPath)
-        print("chechedName:",checkName(newPath))
+        #print(newPath + newFileName)
+        #print("try create dir:" + newPath)
+        #print("chechedName:",checkName(newPath))
         os.makedirs(newPath, mode=0o777, exist_ok=False)
         os.rename(srcDir+delim+mp3, newFileName)
         shutil.move(newFileName, newPath)
@@ -101,11 +100,5 @@ for mp3 in mp3s:
         # <путь до исходного файла> -> <путь до файла результата>;
         # print(path+'\\'+mp3)
         # ./ Duhast.mp3 -> / home / user / some / directory / Rammstein / Sehnsucht / Duhast - Rammstein - Sehnsucht.mp3
-    #except Exception:
-    #    print("error")
-
-
-    #shutil.move(src, dst, copy_function=copy2)
-
 
     #log = '{}'
